2a0dc6551b9a09b098c15cdd89623af988e24a63
FeelipePM <feelipe.pm@gmail.com>
2022-09-15 12:50:08 -0300

    function to handle linkComment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,17 @@
+import { useState } from 'react'
 import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
 export function Comment ({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0)
+
+  const handleLikeComment = () => {
+    setLikeCount((state) => {
+      return state + 1
+    })
+  }
+
   function handleDeleteComment () {
     onDeleteComment(content)
   }
@@ -24,9 +33,9 @@ export function Comment ({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
